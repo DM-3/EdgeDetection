9ead5275803b0bd0d7f16ca3f8bcba958b10d6cc
Martin Werner <m.werner.0@protonmail.com>
2023-06-09 19:23:35 +0200

    Define class Image in image.h

---
+++ src/image.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+template<typename T>
+class Image {
+public:
+    Image(uint32_t t_width, uint32_t t_height);
+    Image(std::string t_load_path);
+    ~Image();
+
+    void load(std::string t_load_path);
+    void store(std::string t_store_path);
+
+    uint32_t getWidth();
+    uint32_t getHeight();
+
+private:
+    uint32_t width;
+    uint32_t height;
+    T* arr;
+};
